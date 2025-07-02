6c096790e9bde3d888b2938887402ace98cbd70b
oshan-2 <oshankhati567@gmail.com>
2025-07-02 17:57:47 +0530

    first commit

--- aquasense-backend/server.js
+++ aquasense-backend/server.js
@@ -20,3 +20,6 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
     });
   })
   .catch(err => console.error(err));
+app.get('/', (req, res) => {
+  res.send('🌊 AQUASENSE Backend is running.');
+});
